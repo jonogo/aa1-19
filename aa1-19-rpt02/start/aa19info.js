567eff2279cc377435de38e426df3fd264dc0a09
jonogo <55525267+jonogo@users.noreply.github.com>
2020-09-17 16:46:39 +0900

    Add files via upload

---
+++ aa1-19-rpt02/start/aa19info.js
@@ -0,0 +1,6 @@
+module.exports = function (id, name, phone){
+console.log("My info");
+console.log("Id : " + id);
+console.log("Name : " + name);
+console.log("Phone : " + phone + "\n");
+};
---
+++ aa1-19-rpt02/start/index_aa19.js
@@ -0,0 +1,4 @@
+var info = require("./aa19info.js");
+
+info("aa19", "jongeya", "010-7504-4443");
+info("aa19", "jongE", "010-4937-4024");
